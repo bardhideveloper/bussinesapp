9feb80b066a77de94143c13038c9a7c5fb148c1e
Bardh Ahmeti <bardh.ahmeti@harrisia.com>
2023-12-04 13:16:28 +0100

    nav-position block for admin panel

--- src/App.js
+++ src/App.js
@@ -38,103 +38,115 @@ function App() {
 
   return (
     <Router>
-      <div className="container">
-        <nav className="pt-3">
-          <ul className="nav nav-tabs fs-5">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-2">
             {userRole === 1 && (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/adminPanel/dashboard">📊 Dashboard</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/adminPanel/users">👥 Users</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/adminPanel/products">🛍️ Products</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/adminPanel/orders">📦 Orders</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/adminPanel/contactus">✉️ Messages</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/dashboardUser" onClick={handleLogout}>📴 Logout</Link>
-                </li>
-
-              </>
+              <div className="admin-nav">
+                <nav className="pt-3">
+                  <ul className="nav fs-5 d-block">
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/adminPanel/dashboard">📊 Dashboard</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/adminPanel/users">👥 Users</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/adminPanel/products">🛍️ Products</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/adminPanel/orders">📦 Orders</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/adminPanel/contactus">✉️ Messages</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/dashboardUser" onClick={handleLogout}>📴 Logout</Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
             )}
+            </div>
 
             {userRole === 2 && (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/userPanel/dashboardUser">🏠 Dashboard</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/order-items">📦 Order Items</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/contactUs">📞 Contact Us</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/aboutUs">ℹ️ About Us</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/dashboardUser" onClick={handleLogout}>📴 Logout</Link>
-                </li>
-              </>
+              <div className="user-nav">
+                <nav className="pt-3">
+                  <ul className="nav fs-5">
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/userPanel/dashboardUser">🏠 Dashboard</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/order-items">📦 Order Items</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/contactUs">📞 Contact Us</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/aboutUs">ℹ️ About Us</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/dashboardUser" onClick={handleLogout}>📴 Logout</Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
             )}
 
             {userRole !== 1 && userRole !== 2 && (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/dashboardUser">🏠 Dashboard</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/contactUs">📞 Contact Us</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/aboutUs">ℹ️ About Us</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/registrationform">📝 Register</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/loginform">🚪 Login</Link>
-                </li>
-
-              </>
+              <div className="default-nav">
+                <nav className="pt-3">
+                  <ul className="nav fs-5">
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/dashboardUser">🏠 Dashboard</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/contactUs">📞 Contact Us</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/aboutUs">ℹ️ About Us</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/registrationform">📝 Register</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/loginform">🚪 Login</Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
             )}
-          </ul>
-        </nav>
+          
 
-        <Routes>
+          <div className="col-md-10">
+            <Routes>
+              <Route path="/" element={<Navigate to={userRole === 1 ? '/adminPanel/dashboard' : (userRole === 2 ? '/userPanel/dashboardUser' : '/dashboardUser')} />} />
+              <Route path="/loginform" element={<LoginForm setUserRole={setUserRole} setUser={setUser} />} />
 
-          <Route path="/" element={<Navigate to={userRole === 1 ? '/adminPanel/dashboard' : (userRole === 2 ? '/userPanel/dashboardUser' : '/dashboardUser')} />} />
-          <Route path="/loginform" element={<LoginForm setUserRole={setUserRole} setUser={setUser} />} />
+              {userRole === 1 && (
+                <Route path="/adminPanel/*">
+                  <Route path="dashboard" element={<Dashboard />} />
+                  <Route path="users" element={<UserList />} />
+                  <Route path="orders" element={<OrdersList />} />
+                  <Route path="products" element={<ProductList />} />
+                  <Route path="contactus" element={<ContactUsList />} />
+                </Route>
+              )}
 
-          {userRole === 1 && (
-            <Route path="/adminPanel/*">
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="users" element={<UserList />} />
-              <Route path="orders" element={<OrdersList />} />
-              <Route path="products" element={<ProductList />} />
-              <Route path="contactus" element={<ContactUsList />} />
-            </Route>
-          )}
+              {userRole === 2 && (
+                <Route path="/userPanel/*">
+                  <Route path="dashboardUser" element={<DashboardUser />} />
+                </Route>
+              )}
 
-          {userRole === 2 && (
-            <Route path="/userPanel/*">
               <Route path="dashboardUser" element={<DashboardUser />} />
-            </Route>
-          )}
-
-          <Route path="dashboardUser" element={<DashboardUser />} />
-          <Route path="/order-items" element={<MakeOrder user={user} />} />
-          <Route path="/registrationform" element={<RegistrationForm />} />
-          <Route path="/aboutUs" element={<AboutUs />} />
-          <Route path="/contactUs" element={<ContactUs />} />
-        </Routes>
+              <Route path="/order-items" element={<MakeOrder user={user} />} />
+              <Route path="/registrationform" element={<RegistrationForm />} />
+              <Route path="/aboutUs" element={<AboutUs />} />
+              <Route path="/contactUs" element={<ContactUs />} />
+            </Routes>
+          </div>
+        </div>
       </div>
     </Router>
   );
